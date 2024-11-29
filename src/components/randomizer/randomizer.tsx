d044a5869802c838af881ba14164f22db80b9a7b
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-11-29 16:01:53 -0500

    Got tested version of book randomizer to work

--- src/components/randomizer/randomizer.tsx
+++ src/components/randomizer/randomizer.tsx
@@ -1,23 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import Header from "../Header";
 import Footer from "../Footer";
 
-export default function randomizer() {
-  useEffect(() => {
-    fetch('https://openlibrary.org/search.json?q=the+lord+of+the+rings')
-      .then(res => res.json())
-      .then((books) => {
-        console.log(books)
-      })
-  })
+export default function Randomizer() {
+
+  const [bookCover, setBookCover] = useState(<img></img>)
+
+
+  const fetchBooks = () => fetch('https://openlibrary.org/search.json?q=the+lord+of+the+rings')
+    .then((res) => res.json())
+    .then((books) => {
+      console.log(books.docs[0].isbn[1])
+      const img = <img src={`https://covers.openlibrary.org/b/isbn/${books.docs[0].isbn[3]}-L.jpg`}></img>
+      setBookCover(img)
+    })
 
   return (
     <>
       <Header />
       <div className="flex flex-col min-h-screen justify-center items-center">
         <h1>Get A Random Book!</h1>
-        <li className="border-[.5em] w-[12em] h-[19em] m-3 list-none"></li>
-        <button className="bg-[#758173] text-[#d9d9d9] p-3 rounded w-48 uppercase bold">Randomize</button>
+        <li className="w-[12em] h-[75%] m-3 list-none book">{bookCover}</li>
+        <button
+          className="bg-[#758173] text-[#d9d9d9] p-3 rounded w-48 h-[25%] uppercase bold"
+          onClick={(() => { fetchBooks() })}
+        >
+          Randomize
+        </button>
       </div>
       <Footer />
     </>
