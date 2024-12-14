09fbd074cef51dd7a26db3e9d05ed78b26943d80
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-12-14 13:29:55 -0500

    .

--- src/app/randomizer/page.tsx
+++ src/app/randomizer/page.tsx
@@ -1,12 +1,13 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Randomizer() {
-
+     
+    // Gets random book when button is clicked, book shows about the same information as the search query
 
     return (
         <div>
             <h1>Title</h1>
-            <Image alt="Book Cover" src="">Book Cover</Image>
+            {/* <Image alt="Book Cover" src="#"></Image> */}
             <p>Author</p>
             <button>Get A Random Book</button>
         </div>
