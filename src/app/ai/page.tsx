62335bb2a9b0456c6e030d66965b690dc52e483e
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-12-17 15:13:44 -0500

    Figured out how to successfully make the AI page, just needs styling and some error handling

--- src/app/ai/page.tsx
+++ src/app/ai/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import OpenAI from "openai";
 import { useState } from "react";
@@ -6,45 +6,66 @@ import Header from "../(components)/Header";
 import Footer from "../(components)/Footer";
 
 export default function AI() {
-
-    const [prompt, setPrompt] = useState('')
-    const [recommendation, setRecommendation] = useState('')
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null); 
-
-
+  const [prompt, setPrompt] = useState<string | null>(null);
+  const [recommendation, setRecommendation] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const openai = new OpenAI({
     apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
     dangerouslyAllowBrowser: true,
   });
 
-  try {
-      async function open() {
-        const completion = await openai.chat.completions.create({
-          model: "",
-          messages: [
-            { role: "system", content: "You are a book recommendation assistant" },
+  //   const handleSubmit = async (e) => {
+  //     e.preventDefault();
+  //     setLoading(true);
+
+  const generateResponse = async (inputText: string) => {
+    const completion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: [
             {
-              role: "user",
-              content: "Give me a book recommandation based on Norwegian Wood",
+              type: "text",
+              text: "You are a book recommendation assistant, strictly only give book recommendations nothing",
             },
           ],
-        });
-        console.log(completion.choices[0].message);
-      }
-  } catch {
-    setError(error)
-  }
+        },
+        {
+          role: "user",
+          content: [
+            {
+              type: "text",
+              text: inputText,
+            },
+          ],
+        },
+      ],
+    });
+    setRecommendation(inputText);
+    setPrompt(completion.choices[0].message.content);
+  };
   // Using OpenAI a user will be able to provide a prompt for the AI to generate and show a list of books from openlibrary that best match
   // what is asked
 
   return (
     <div>
       <Header />
-      <h1>AI</h1>
-      <textarea></textarea>
-      <button onClick={() => open()}></button>
+      <div className="min-h-screen justify-items-center">
+        <h1>AI</h1>
+        <textarea
+          placeholder="Give me a book similar to......"
+          className="block w-[75%] p-2.5"
+          value={recommendation}
+          onChange={e => setRecommendation(e.target.value)}
+        ></textarea>
+        <button onClick={() => generateResponse(recommendation)}>
+          Generate Book
+        </button>
+        <div>{prompt}</div>
+      </div>
       <Footer />
     </div>
   );
