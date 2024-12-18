c1233e4b8bf5726096d89f0e5d45d7bc4309f388
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-12-18 14:42:22 -0500

    Fixed randomizer a bit

--- src/app/randomizer/page.tsx
+++ src/app/randomizer/page.tsx
@@ -11,41 +11,46 @@ export default function Randomizer() {
   const [bookCover, setBookCover] = useState(<img></img>);
   const [isLoading, setIsLoading] = useState(false);
 
-  try {
-    fetch("https://openlibrary.org/search.json?q=random&fields=*")
-      .then((res) => res.json())
-      .then((books) => {
-        const randomBook = Math.floor(Math.random() * books.docs.length);
-        let src = ``;
-        src = `https://covers.openlibrary.org/b/olid/${books.docs[randomBook].cover_edition_key}-L.jpg`;
-        const img = (
-          <Image
-            loader={() => src}
-            src={src}
-            alt="Book cover"
-            width={200}
-            height={400}
-          />
-        );
-        setBookCover(img);
-        setIsLoading(true);
-      });
-  } catch {
-    console.log("cant fetch books...trying again");
+  async function randomBook() {
+    try {
+      setIsLoading(true);
+      await fetch("https://openlibrary.org/search.json?q=random&fields=*")
+        .then((res) => res.json())
+        .then((books) => {
+          const randomBook = Math.floor(Math.random() * books.docs.length);
+          let src = ``;
+          src = `https://covers.openlibrary.org/b/olid/${books.docs[randomBook].cover_edition_key}-L.jpg`;
+          const img = (
+            <Image
+              loader={() => src}
+              src={src}
+              alt="Book cover"
+              width={200}
+              height={400}
+            />
+          );
+          setBookCover(img);
+        });
+    } catch {
+      console.log("cant fetch books...trying again");
+    } finally {
+      setIsLoading(false);
+    }
   }
-
   return (
     <div>
       <Header />
       <div className="min-h-screen content-center justify-items-center">
         <h1>Title</h1>
         {isLoading ? (
-          bookCover
-        ) : (
           <li className="w-[12.75em] h-[21em] m-10 animate-pulse list-none bg-white"></li>
+        ) : (
+          bookCover
         )}
         <p>Author</p>
-        <button>Get A Random Book</button>
+        <button onClick={() => randomBook()}>
+          {isLoading ? "Generating...." : "Get a random book"}
+        </button>
       </div>
       <Footer />
     </div>
