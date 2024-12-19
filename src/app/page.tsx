e2167bbce9988cce2bacde639cd617f072507bcf
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-12-19 16:03:18 -0500

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import Header from "@/app/(components)/Header";
 import Footer from "@/app/(components)/Footer";
 import Image from "next/image";
@@ -10,8 +10,22 @@ import Link from "next/link";
 export default function Page() {
   // Gets randomly selected books and does a scrolling animation for them
   // Search bar allowing you to search from author, title, isbn, etc.
-  const [bookCover, setBookCover] = useState<React.JSX.Element | undefined>();
+  const [books, setBooks] = useState<React.JSX.Element | undefined>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setIsLoading(true);
+    setError(null);
+    try {
+    } catch (error: any) {
+      setError(error.message)
+      console.log(error)
+    } finally {
+      setIsLoading(true)
+    }
+  }
 
   useEffect(() => {
     const fetchRandomBook = () => {
@@ -19,7 +33,7 @@ export default function Page() {
         fetch("https://openlibrary.org/search.json?q=random&fields=*")
           .then((res) => res.json())
           .then((books) => {
-            books.docs.slice(0, 8). map((book: any) => {
+            books.docs.slice(0, 8).map((book: any) => {
               let src = ``;
               src = `https://covers.openlibrary.org/b/olid/${book.cover_edition_key}-L.jpg`;
               const img = (
@@ -32,7 +46,7 @@ export default function Page() {
                   height={700}
                 />
               );
-              setBookCover(img);
+              setBooks(img);
               setIsLoading(true);
             });
           });
@@ -53,50 +67,53 @@ export default function Page() {
         <div className="w-[calc(550px*8)] animate-scroll">
           <ul className="grid grid-cols-12 justify-items-center whitespace-nowrap">
             {isLoading ? (
-              <li className="w-[50%] h-[100%] m-10">{bookCover}</li>
+              <li className="w-[50%] h-[100%] m-10">{books}</li>
             ) : (
               <li className="w-[12.75em] h-[21em] m-10 animate-pulse bg-white"></li>
             )}
             {isLoading ? (
-              <li className="w-[50%] h-[100%] m-10">{bookCover}</li>
+              <li className="w-[50%] h-[100%] m-10">{books}</li>
             ) : (
               <li className="w-[12.75em] h-[21em] m-10 animate-pulse bg-white"></li>
             )}
             {isLoading ? (
-              <li className="w-[50%] h-[100%] m-10">{bookCover}</li>
+              <li className="w-[50%] h-[100%] m-10">{books}</li>
             ) : (
               <li className="w-[12.75em] h-[21em] m-10 animate-pulse bg-white"></li>
             )}
             {isLoading ? (
-              <li className="w-[50%] h-[100%] m-10">{bookCover}</li>
+              <li className="w-[50%] h-[100%] m-10">{books}</li>
             ) : (
               <li className="w-[12.75em] h-[21em] m-10 animate-pulse bg-white"></li>
             )}
             {isLoading ? (
-              <li className="w-[50%] h-[100%] m-10">{bookCover}</li>
+              <li className="w-[50%] h-[100%] m-10">{books}</li>
             ) : (
               <li className="w-[12.75em] h-[21em] m-10 animate-pulse bg-white"></li>
             )}
             {isLoading ? (
-              <li className="w-[50%] h-[100%] m-10">{bookCover}</li>
+              <li className="w-[50%] h-[100%] m-10">{books}</li>
             ) : (
               <li className="w-[12.75em] h-[21em] m-10 animate-pulse bg-white"></li>
             )}
             {isLoading ? (
-              <li className="w-[50%] h-[100%] m-10">{bookCover}</li>
+              <li className="w-[50%] h-[100%] m-10">{books}</li>
             ) : (
               <li className="w-[12.75em] h-[21em] m-10 animate-pulse bg-white"></li>
             )}
             {isLoading ? (
               <li className="w-[50%] h-[100%] m-10">
-                <Link href={`/books/id`}>{bookCover}</Link>
+                <Link href={`/books/id`}>{books}</Link>
               </li>
             ) : (
               <li className="w-[12.75em] h-[21em] m-10 animate-pulse bg-white"></li>
             )}
           </ul>
         </div>
-        <div className="flex justify-center text-[#A9C5A0] mt-12">
+        <form
+          className="flex justify-center text-[#A9C5A0] mt-12"
+          onSubmit={onSubmit}
+        >
           <input
             type="text"
             placeholder="Search books by title or author"
@@ -105,7 +122,7 @@ export default function Page() {
           <button className="bg-[#D9D9D9] text-[#000] md:text-2xl sm:text-sm p-5">
             <FaSearch />
           </button>
-        </div>
+        </form>
       </div>
       <Footer />
     </React.Fragment>
