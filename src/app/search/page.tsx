e044c0c0f696ce895bda929c6dcad01a6579d662
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-12-24 10:20:09 -0500

    Just added on click for load more button, no functionality yet

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -94,7 +94,7 @@ function SearchResults() {
               </Link>
             ))}
       </div>
-      <button className="focus:outline-none text-white bg-[#3c7a46] hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 my-5 dark:bg-[#3c7a46] dark:hover:bg-green-700 dark:focus:ring-green-800">
+      <button onClick={() => loadMoreBtn()}className="focus:outline-none text-white bg-[#3c7a46] hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 my-5 dark:bg-[#3c7a46] dark:hover:bg-green-700 dark:focus:ring-green-800">
         Load More
       </button>
     </div>
