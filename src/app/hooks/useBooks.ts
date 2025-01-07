f31071377735c80d1ac19719f603e4ea20d29630
jewelsonmyjeans <aviadchuraman@gmail.com>
2025-01-07 14:47:14 -0500

    Going to add a switch statement depending on where this is being implemented (home page, search page, author page, etc

--- src/app/hooks/useBooks.ts
+++ src/app/hooks/useBooks.ts
@@ -1,9 +1,23 @@
 import { useEffect, useState } from "react";
 
+type FetchType = "random" | "search" | "author" | "genre";
+
 export function useBooks() {
   const [books, setBooks] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [hasMore, setHasMore] = useState<boolean>(true);
 
+  useEffect(() => {
+    const fetchBooks = async () => {
+      setIsLoading(true);
+      setError(null);
+
+      try {
+      } catch {
+      } finally {
+      }
+    };
+  }, []);
   return { books, isLoading, error };
 }
