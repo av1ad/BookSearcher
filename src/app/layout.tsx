4d6d2837c908fedf53da2973b6e694309822bf4c
jewelsonmyjeans <aviadchuraman@gmail.com>
2025-01-28 06:18:40 -0500

    Speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "@/app/styles/globals.css";
 
 export const metadata: Metadata = {
@@ -59,6 +60,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <SpeedInsights />
       <Analytics />
     </html>
   );
