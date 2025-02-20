86680e0bd57f6e22ca5bcb01ca14a6803cde5cfe
av1ad <aviadchuraman@gmail.com>
2025-02-20 14:56:28 -0500

    Fixed padding on footer so it's not too small

--- src/app/(components)/Footer.tsx
+++ src/app/(components)/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-      <footer className="w-full p-1 bg-[#758173] bg-opacity-30 shadow md:justify-between md:p-1 mt-24 text-center">
+      <footer className="w-full p-3 bg-[#758173] bg-opacity-30 shadow md:justify-between md:p-3 mt-24 text-center">
         <a className="text-[#A9C5A0]" href="https://github.com/av1ad">Aviad Churaman © 2025</a>
       </footer>
     );
